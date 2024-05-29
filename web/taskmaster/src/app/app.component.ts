5913b339b4b593a27b321fed05b4f4b5743876c5
Andres Javier Lopez <code@andresjavierlopez.com>
2024-05-29 12:59:19 -0600

    Add task list to main page

--- web/taskmaster/src/app/app.component.ts
+++ web/taskmaster/src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'taskmaster';
+  tasks = [
+    {
+      'uuid': '1234',
+      'title': 'test 1',
+      'description': 'this is a task',
+      'status': 'pending',
+      'due': null,
+      'mood': 'neutral'
+    },
+    {
+      'uuid': '4567',
+      'title': 'test 2',
+      'description': 'this is another task',
+      'status': 'pending',
+      'due': null,
+      'mood': 'neutral'
+    }
+  ];
 }
