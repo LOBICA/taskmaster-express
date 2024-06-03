9a5011acdb35e3f7a690fe07df72b91d39cb9295
Andres Javier Lopez <code@andresjavierlopez.com>
2024-06-03 16:56:48 -0600

    Add environments configuration

--- web/taskmaster/src/app/task.service.ts
+++ web/taskmaster/src/app/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from "@angular/core";
 import { Task } from "./task.model";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "../environments/environment";
 
 @Injectable({
     providedIn: 'root'
@@ -10,10 +11,10 @@ export class TaskService {
     http = inject(HttpClient)
 
     getTasks(): Observable<Task[]> {
-         return this.http.get<Task[]>('http://localhost:8001/tasks')
+         return this.http.get<Task[]>(environment.apiUrl + '/tasks')
     }
 
     addTask(task: Task): Observable<Task> {
-        return this.http.post<Task>('http://localhost:8001/tasks', task);
+        return this.http.post<Task>(environment.apiUrl + '/tasks', task);
     }
 }
