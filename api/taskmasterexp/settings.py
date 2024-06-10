8a17a04740fb91335c881a705f01eac9e785e458
Andres Javier Lopez <code@andresjavierlopez.com>
2024-06-10 09:24:20 -0600

    Add dependencies and configuration

--- api/taskmasterexp/settings.py
+++ api/taskmasterexp/settings.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 PSQL_CONNECTION_STRING = os.getenv("PSQL_CONNECTION_STRING")
