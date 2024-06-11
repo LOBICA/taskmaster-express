6acf7d805b99f78fbf2c2434b449ebc5397d5b0b
Andres Javier Lopez <code@andresjavierlopez.com>
2024-06-10 22:13:12 -0600

    Reformat task service

--- web/taskmaster/src/app/services/task.service.ts
+++ web/taskmaster/src/app/services/task.service.ts
@@ -1,32 +1,37 @@
-import { Injectable } from "@angular/core";
-import { Task } from "../models/task.model";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { environment } from "../../environments/environment";
+import { Injectable } from '@angular/core';
+import { Task } from '../models/task.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getTasks(status="pending"): Observable<Task[]> {
-         return this.http.get<Task[]>(environment.apiUrl + '/tasks?status=' + status)
-    }
+  getTasks(status = 'pending'): Observable<Task[]> {
+    return this.http.get<Task[]>(
+      environment.apiUrl + '/tasks?status=' + status,
+    );
+  }
 
-    addTask(task: Task): Observable<Task> {
-        return this.http.post<Task>(environment.apiUrl + '/tasks', task);
-    }
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(environment.apiUrl + '/tasks', task);
+  }
 
-    getTask(task_id: string): Observable<Task> {
-        return this.http.get<Task>(environment.apiUrl + '/tasks/' + task_id)
-    }
+  getTask(task_id: string): Observable<Task> {
+    return this.http.get<Task>(environment.apiUrl + '/tasks/' + task_id);
+  }
 
-    editTask(task_id: string, task: Task): Observable<Task> {
-        return this.http.patch<Task>( environment.apiUrl + '/tasks/' + task_id, task)
-    }
+  editTask(task_id: string, task: Task): Observable<Task> {
+    return this.http.patch<Task>(
+      environment.apiUrl + '/tasks/' + task_id,
+      task,
+    );
+  }
 
-    deleteTask(task_id: string): Observable<null> {
-        return this.http.delete<null>(environment.apiUrl + '/tasks/' + task_id)
-    }
+  deleteTask(task_id: string): Observable<null> {
+    return this.http.delete<null>(environment.apiUrl + '/tasks/' + task_id);
+  }
 }
