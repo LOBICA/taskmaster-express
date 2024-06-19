eaf49adaa42d4890a82c1640dbbb4101be55aad5
Carlos Lopez <carlos.alopezfigueroa@gmail.com>
2024-06-19 11:52:59 -0600

    change deps on initializer

--- web/taskmaster/src/app/app.config.ts
+++ web/taskmaster/src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { loaderInterceptor } from './interceptors/loader.interceptor';
 import { authenticationInterceptor } from './interceptors/authentication.interceptor';
 import { appInitializer } from '../facebook/fbSDK';
+import { LoginService } from './services/login.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,7 +22,7 @@ export const appConfig: ApplicationConfig = {
       provide: APP_INITIALIZER,
       useFactory: appInitializer,
       multi: true,
-      deps: [HttpClient],
+      deps: [LoginService],
     },
   ]
 };
