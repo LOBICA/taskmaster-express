b5c5bb70dc47ac2f957e37f287dfc1694e6e7e96
Carlos Lopez <carlos.alopezfigueroa@gmail.com>
2024-06-22 21:18:49 -0600

    revert custom webpack and add new attempt to use env file

--- web/taskmaster/src/environments/environment.development.ts
+++ web/taskmaster/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8001',
-    facebookAppId: '821367659917526',
+    facebookAppId: process.env["FB_CLIENT_ID"],
     deepLink: `oauth?client_id=${process.env["821367659917526"]}&redirect_uri=${process.env["FB_REDIRECT"]}&scope=email&response_type=token`,
 };
--- web/taskmaster/src/environments/environment.ts
+++ web/taskmaster/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.helpitdone.com',
-    facebookAppId: '821367659917526',
-    deepLink: `oauth?client_id=${process.env["821367659917526"]}&redirect_uri=${process.env["FB_REDIRECT"]}&scope=email`,
+    facebookAppId: process.env["FB_CLIENT_ID"],
+    deepLink: `oauth?client_id=${process.env["FB_CLIENT_ID"]}&redirect_uri=${process.env["FB_REDIRECT"]}&scope=email&response_type=token`,
 };
