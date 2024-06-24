0a183393c2b9bef42968685f3daae129b7a67257
Carlos Lopez <carlos.alopezfigueroa@gmail.com>
2024-06-23 21:10:34 -0600

    add snackbar service

---
+++ web/taskmaster/src/app/components/custom-snackbar/custom-snackbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-snackbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-snackbar.component.html',
+  styleUrl: './custom-snackbar.component.scss'
+})
+export class CustomSnackbarComponent {
+
+}
