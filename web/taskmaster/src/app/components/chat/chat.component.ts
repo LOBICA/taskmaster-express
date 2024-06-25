1bd9f9fbecd43fe2a1b218d8f90e66454c8a7c66
Andres Javier Lopez <code@andresjavierlopez.com>
2024-06-25 15:41:00 -0600

    Add final improvements

--- web/taskmaster/src/app/components/chat/chat.component.ts
+++ web/taskmaster/src/app/components/chat/chat.component.ts
@@ -37,7 +37,7 @@ export class ChatComponent {
       if (event.data) {
         const message = JSON.parse(event.data) as Message;
         this.messages.push(message);
-        this.chatService.messageReceived$.next(true);
+        this.chatService.messageReceived$.next();
       }
     });
   }
