97c78ea4998cd0bebe746de49adbc998594d43b2
Andres Javier Lopez <code@andresjavierlopez.com>
2024-08-12 18:03:26 -0600

    Add paypal sandbox credentials to test environment

--- web/taskmaster/src/environments/environment.test.ts
+++ web/taskmaster/src/environments/environment.test.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://test.api.helpitdone.com',
   facebookAppId: '821367659917526',
-  paypalClientId: '',
-  paypalPlanId: '',
+  paypalClientId: 'ATvXagOAdENkD5l22kE3Xl1ssZcz_97BGXm0ekPfCGA-FxcdM_TiHECkl6kZysU4C-JmUuMxExoM2EvW',
+  paypalPlanId: 'P-62K651209R182263BM2SDEAY',
 };
