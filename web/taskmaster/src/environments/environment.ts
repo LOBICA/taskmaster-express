97df633317b1ed976020a89f66a3cfa3101378e6
Andres Javier Lopez <code@andresjavierlopez.com>
2024-08-17 18:47:44 -0600

    Add client id and plan to production

--- web/taskmaster/src/environments/environment.ts
+++ web/taskmaster/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     apiUrl: 'https://api.helpitdone.com',
     facebookAppId: '821367659917526',
-    paypalClientId: '',
-    paypalPlanId: '',
+    paypalClientId: 'AVAnn82XeJS_M3IP9pJwhlVasr8CXtDruK1L4QWwwlbWYPbKuTQa0xpgEFtaPOn3Yh6KBiCSIoG-IwX_',
+    paypalPlanId: 'P-9L235380533672425M3AUIYY',
 };
