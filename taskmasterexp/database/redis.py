267ca4f4c748fd3588a3c2fb3b7f63e0ec3d8e9a
Andres Javier Lopez <code@andresjavierlopez.com>
2024-10-02 12:20:33 -0400

    Disable SSL requirements for Redis

--- taskmasterexp/database/redis.py
+++ taskmasterexp/database/redis.py
@@ -4,7 +4,7 @@ from taskmasterexp.settings import REDIS_URL
 
 
 async def get_redis():
-    client = redis.from_url(REDIS_URL)
+    client = redis.from_url(REDIS_URL, ssl_cert_reqs=None)
     async with client as conn:
         await conn.ping()
         yield client
