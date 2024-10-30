6672536fcfbb49eebb1b32720a3fc83bec8ccda8
Andres Javier Lopez <code@andresjavierlopez.com>
2024-10-30 15:37:56 -0400

    Add error message for seed database

--- taskmasterexp/database/cli.py
+++ taskmasterexp/database/cli.py
@@ -1,6 +1,7 @@
 import asyncio
 
 import click
+from sqlalchemy.exc import IntegrityError
 
 from .seed import seed as seed_process
 
@@ -8,5 +9,8 @@ from .seed import seed as seed_process
 @click.command()
 def seed():
     click.echo("Seeding database...")
-    asyncio.run(seed_process())
+    try:
+        asyncio.run(seed_process())
+    except IntegrityError:
+        click.echo("Could not seed database. Database may be already seeded.")
     click.echo("Done!")
