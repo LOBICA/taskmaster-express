774aa4fff53c599049a1ead61102f97f07c9a794
Andres Javier Lopez <code@andresjavierlopez.com>
2024-11-07 16:03:49 -0500

    Use an object instead of a single function call to get the model

--- taskmasterexp/ai/demo/assistant.py
+++ taskmasterexp/ai/demo/assistant.py
@@ -7,7 +7,7 @@ from langchain_core.tools import tool
 from taskmasterexp.schemas.users import User
 from taskmasterexp.settings import DEMO_TOPIC
 
-from ..client import get_chat_model
+from ..model import get_chat_model
 
 logger = logging.getLogger(__name__)
 
