22b16ce535f4f228e73a0b1c6d16eaa202c1ba0e
Andres Javier Lopez <code@andresjavierlopez.com>
2025-02-03 18:17:44 -0600

    Update missing references

--- scripts/launch-api.prod.sh
+++ scripts/launch-api.prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python -m alembic upgrade head
-python -m uvicorn taskmasterexp.app:app --host 0.0.0.0 --port $PORT
+python -m uvicorn taskmaster.app:app --host 0.0.0.0 --port $PORT
